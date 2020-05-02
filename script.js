f2589376267e80a734a9480e48b62f2e5c44a349
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-05-02 15:02:54 -0400

    initial commit for js

---
+++ script.js
@@ -0,0 +1,63 @@
+// scheduled events
+
+rows : {},
+
+
+// classname
+
+className:'jq-schedule',
+
+// schedule start time(HH:ii)
+
+startTime:"07:00",
+
+// schedule end time(HH:ii)
+
+endTime:"21:00", 
+
+ 
+
+// width(px)
+
+widthTimeX: 25,
+
+// cell timestamp example 10 minutes
+
+widthTime: 60 * 10, 
+
+// height(px)
+
+timeLineY: 60,   
+
+// options for time slots
+
+timeLineBorder:1,
+
+timeBorder:1,  // border width
+
+timeLinePaddingTop:0,
+
+timeLinePaddingBottom:0,
+// time border width 
+
+
+// data width
+
+dataWidth:160, 
+
+
+vertical<a href="https://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>bar: 0,
+
+// width to move all schedules to the right of the clicked time cell
+
+bundleMoveWidth: 1,
+
+ 
+
+// draggable?
+
+draggable:true,
+
+// resizable?
+
+resizable:true
