d783b94bd574bfd3f2b2b4b4185034a94e8023a6
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-05-05 18:01:22 -0400

    fixed local storage and time tracker

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
-// scheduled events
+var timeSlots = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+var militaryTime= ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
-rows : {},
 
 
-// classname
+$(document).ready(function() {
+    function currentTime() {
+        var todaysDate = moment().format("LLLL");
+        console.log(todaysDate);
+        $("#currentDay").append("Today is  " + todaysDate); 
+      }
+      currentTime();
+      console.log(currentTime);
 
-className:'jq-schedule',
 
-// schedule start time(HH:ii)
+      $('button').on("click", function saveIt() {
+        var userInput = $(this).parent().attr("id");
+        var textContent = $(this).siblings('input').val();
+        
+        localStorage.setItem(userInput, textContent);
+        //console.log(userInput, textContent);
+        
+        });
 
-startTime:"07:00",
+        updateTime()
 
-// schedule end time(HH:ii)
+        function updateTime() {
+            var timeNow = moment().format('H');
+            for(var i = 0; i < timeSlots.length; i++) {
+                console.log('time now: ', timeNow)
+                console.log('timeSlots: ', timeSlots[i])
+              if (militaryTime[i] > timeNow) {
+                $("#" + timeSlots[i]).attr("style", "background-color: #77dd77");
+              } else if (militaryTime[i] < timeNow) {
+                $("#" + timeSlots[i]).attr("style", "background-color: #d3d3d3");
+              } else if (militaryTime[i] == timeNow) {
+                $("#" + timeSlots[i]).attr("style", "background-color: #ff6961");
+                //console.log(("#" + timeSlots[i]).attr("style", "background-color: #fc665e"))
+              }
+            }
+          };
 
-endTime:"21:00", 
+        
+       
+        $("#nine").val(localStorage.getItem("9am"))
+          console.log(nine)
+        $("#ten").val(localStorage.getItem("10am"))
+          console.log(ten)
+        $("#eleven").val(localStorage.getItem("11am"))
+          console.log(eleven)
+        $("#twelve").val(localStorage.getItem("12pm"))
+          console.log(twelve)
+        $("#one").val(localStorage.getItem("1pm"))
+          console.log(one)
+        $("#two").val(localStorage.getItem("2pm"))
+          console.log(two)
+        $("#three").val(localStorage.getItem("3pm"))
+          console.log(three)
+        $("#four").val(localStorage.getItem("4pm"))
+          console.log(four)
+        $("#five").val(localStorage.getItem("5pm"))
+          console.log(five)
+
+    });
+   
+
+
 
  
-
-// width(px)
-
-widthTimeX: 25,
-
-// cell timestamp example 10 minutes
-
-widthTime: 60 * 10, 
-
-// height(px)
-
-timeLineY: 60,   
-
-// options for time slots
-
-timeLineBorder:1,
-
-timeBorder:1,  // border width
-
-timeLinePaddingTop:0,
-
-timeLinePaddingBottom:0,
-// time border width 
-
-
-// data width
-
-dataWidth:160, 
-
-
-vertical<a href="https://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>bar: 0,
-
-// width to move all schedules to the right of the clicked time cell
-
-bundleMoveWidth: 1,
-
- 
-
-// draggable?
-
-draggable:true,
-
-// resizable?
-
-resizable:true
+     
+    
